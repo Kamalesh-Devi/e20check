0131b81ccb0c3abb4457b7a85a85855ad6416947
Kamalesh <kamaleshrockz@gmail.com>
2024-12-20 14:46:41 +0000

    Second edit of tax.sh in master

--- tax.sh
+++ tax.sh
@@ -3,6 +3,8 @@ echo "Enter Salary"
 read a
 if [ "$a" -lt 30000 ];then
 echo "No tax"
+elif [ "$a" -eq 30000 ];then
+echo "5% tax"
 elif [ "$a" -gt 30000 -a "$a" -le 40000 ];then
 echo "10% tax"
 else
